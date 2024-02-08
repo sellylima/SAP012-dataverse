002da9bd339ec25e1d199a4c189e95d423803c21
PVCBia <pvcbeatriz@gmail.com>
2024-02-08 14:54:31 -0300

    adição "option = vazio"

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -1,4 +1,355 @@
+//nomeio essa propriedade?
 export default [
+  {
+    "id": "anitta",
+    "nome": "Anitta",
+    "shortDescription": "Cantora e compositora brasileira, sucesso internacional com hits como 'Downtown'.",
+    "description": "Anitta é uma cantora e compositora brasileira que ganhou destaque internacional com seu estilo único e energético. Ela é conhecida por hits como 'Downtown' e 'Vai Malandra', além de ser uma defensora dos direitos LGBTQ+.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "29",
+      "nacionalidade": "Brasileira",
+      "areaDeAtuacao": "Música",
+      "trabalhoMaisRelevante": "Cantora e compositora, sucesso internacional com hits como 'Downtown' e 'Vai Malandra'.",
+      "escandalo": "Rumores de desavenças com outros artistas.",
+      "curiosidade": "É uma defensora dos direitos LGBTQ+."
+    }
+  },
+  {
+    "id": "scarlett-johansson",
+    "nome": "Scarlett Johansson",
+    "shortDescription": "Atriz conhecida por papéis em filmes como 'Lost in Translation' e 'Avengers'.",
+    "description": "Scarlett Johansson é uma aclamada atriz americana conhecida por sua versatilidade e presença cativante na tela. Ela ganhou reconhecimento por papéis em filmes como 'Lost in Translation' e 'Avengers', além de ser uma ativista pelos direitos das mulheres em Hollywood.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "38",
+      "nacionalidade": "Americana",
+      "areaDeAtuacao": "Cinema",
+      "trabalhoMaisRelevante": "Atriz conhecida por papéis em filmes como 'Lost in Translation' e 'Avengers'.",
+      "escandalo": "Controvérsias em escolhas de papéis no cinema.",
+      "curiosidade": "Ativista pelos direitos das mulheres em Hollywood."
+    }
+  },
+  {
+    "id": "bts",
+    "nome": "BTS",
+    "shortDescription": "Grupo de K-pop internacionalmente aclamado.",
+    "description": "BTS é um grupo de K-pop sul-coreano que alcançou sucesso global com seu talento musical e carisma. Eles são reconhecidos por suas coreografias elaboradas, letras significativas e grande presença nas redes sociais, interagindo diretamente com os fãs.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "28",
+      "nacionalidade": "Sul-Coreana",
+      "areaDeAtuacao": "Música (K-pop)",
+      "trabalhoMaisRelevante": "Grupo de K-pop internacionalmente aclamado.",
+      "escandalo": "Rumores constantes de namoro.",
+      "curiosidade": "Grande presença nas redes sociais, interagindo diretamente com os fãs."
+    }
+  },
+  {
+    "id": "elon-musk",
+    "nome": "Elon Musk",
+    "shortDescription": "Empresário e visionário, CEO da Tesla e SpaceX.",
+    "description": "Elon Musk é um empresário sul-africano-americano conhecido por sua visão inovadora e empreendedora. Ele é o CEO da Tesla, SpaceX e outras empresas, e é uma figura central em avanços tecnológicos, incluindo exploração espacial e veículos elétricos.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "52",
+      "nacionalidade": "Sul-Africano (Naturalizado Americano)",
+      "areaDeAtuacao": "Ciência e Tecnologia",
+      "trabalhoMaisRelevante": "Empresário e visionário, CEO da Tesla e SpaceX.",
+      "escandalo": "Mensagens polêmicas no Twitter.",
+      "curiosidade": "Envolvimento em projetos de exploração espacial."
+    }
+  },
+  {
+    "id": "rihanna",
+    "nome": "Rihanna",
+    "shortDescription": "Cantora, empresária de moda e fundadora da Fenty Beauty.",
+    "description": "Rihanna é uma talentosa cantora, empresária de moda e filantropa barbadiana. Além de sua carreira musical de sucesso, ela é fundadora da Fenty Beauty, uma linha de cosméticos celebrada por sua inclusão e diversidade.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "35",
+      "nacionalidade": "Barbadiana",
+      "areaDeAtuacao": "Música e Moda",
+      "trabalhoMaisRelevante": "Cantora, empresária de moda e fundadora da Fenty Beauty.",
+      "escandalo": "Polêmicas em premiações e eventos.",
+      "curiosidade": "Ícone da moda e defensora da diversidade na indústria da beleza."
+    }
+  },
+  {
+    "id": "roger-federer",
+    "nome": "Roger Federer",
+    "shortDescription": "Tenista recordista de títulos de Grand Slam.",
+    "description": "Roger Federer é um tenista suíço amplamente considerado um dos maiores jogadores da história do tênis. Ele quebrou diversos recordes ao longo de sua carreira, conquistando múltiplos títulos de Grand Slam e demonstrando habilidade excepcional nas quadras.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "41",
+      "nacionalidade": "Suíço",
+      "areaDeAtuacao": "Esportes (Tênis)",
+      "trabalhoMaisRelevante": "Tenista recordista de títulos de Grand Slam.",
+      "escandalo": "Envolvimento em controvérsias no mundo do tênis.",
+      "curiosidade": "Atua ativamente em projetos de caridade."
+    }
+  },
+  {
+    "id": "emma-watson",
+    "nome": "Emma Watson",
+    "shortDescription": "Atriz famosa por interpretar Hermione em 'Harry Potter' e ativista pelos direitos das mulheres.",
+    "description": "Emma Watson é uma atriz britânica conhecida por seu papel como Hermione na série de filmes 'Harry Potter'. Além de sua carreira no cinema, ela é uma defensora ativa dos direitos das mulheres, promovendo a igualdade de gênero e a educação em todo o mundo.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "33",
+      "nacionalidade": "Britânica",
+      "areaDeAtuacao": "Cinema e Ativismo",
+      "trabalhoMaisRelevante": "Atriz famosa por interpretar Hermione em 'Harry Potter' e ativista pelos direitos das mulheres.",
+      "escandalo": "Sem grandes polêmicas públicas.",
+      "curiosidade": "Graduada em Literatura Inglesa em uma universidade renomada."
+    }
+  },
+  {
+    "id": "will-smith",
+    "nome": "Will Smith",
+    "shortDescription": "Ator de sucesso em filmes como 'Men in Black' e rapper renomado.",
+    "description": "Will Smith é um aclamado ator americano conhecido por seus papéis em filmes de sucesso como 'Men in Black' e 'Independence Day'. Além de sua carreira no cinema, ele é um rapper renomado e uma figura influente nas redes sociais, compartilhando insights de sua vida.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "55",
+      "nacionalidade": "Americana",
+      "areaDeAtuacao": "Cinema e Música",
+      "trabalhoMaisRelevante": "Ator de sucesso em filmes como 'Men in Black' e rapper renomado.",
+      "escandalo": "Problemas familiares expostos publicamente.",
+      "curiosidade": "Presença marcante nas redes sociais, compartilhando insights de sua vida."
+    }
+  },
+  {
+    "id": "tais-araujo",
+    "nome": "Taís Araújo",
+    "shortDescription": "Atriz premiada, conhecida por seus papéis em novelas e séries brasileiras.",
+    "description": "Taís Araújo é uma renomada atriz brasileira que ganhou destaque por seus papéis em novelas e séries de televisão. Além de sua carreira artística, ela é uma ativista reconhecida por defender os direitos raciais e de gênero no Brasil.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "43",
+      "nacionalidade": "Brasileira",
+      "areaDeAtuacao": "Cinema e TV",
+      "trabalhoMaisRelevante": "Atriz premiada, conhecida por seus papéis em novelas e séries brasileiras.",
+      "escandalo": "Envolvimento em debates sobre representatividade na mídia.",
+      "curiosidade": "Ativa defensora dos direitos raciais e de gênero."
+    }
+  },
+  {
+    "id": "cristiano-ronaldo",
+    "nome": "Cristiano Ronaldo",
+    "shortDescription": "Jogador de futebol, reconhecido como um dos melhores da história.",
+    "description": "Cristiano Ronaldo é um jogador de futebol português amplamente reconhecido como um dos maiores da história do esporte. Ele acumulou numerosos prêmios e recordes ao longo de sua carreira, e é conhecido por seu talento excepcional e dedicação ao jogo.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "38",
+      "nacionalidade": "Portuguesa",
+      "areaDeAtuacao": "Esportes (Futebol)",
+      "trabalhoMaisRelevante": "Jogador de futebol, reconhecido como um dos melhores da história.",
+      "escandalo": "Questões legais relacionadas a impostos.",
+      "curiosidade": "Focado em investimentos e negócios fora do futebol."
+    }
+  },
+  {
+    "id": "lana-del-rey",
+    "nome": "Lana Del Rey",
+    "shortDescription": "Cantora e compositora conhecida por seu estilo retrô e cinematográfico.",
+    "description": "Lana Del Rey é uma cantora e compositora americana que ganhou destaque com o álbum 'Born to Die'. Ela é reconhecida por sua estética vintage e letras melancólicas, embaladas por uma voz única e envolvente.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "36",
+      "nacionalidade": "Americana",
+      "areaDeAtuacao": "Música",
+      "trabalhoMaisRelevante": "\"Born to Die\" é considerado um de seus álbuns mais impactantes, catapultando-a para a fama.",
+      "escandalo": "Em 2020, ela recebeu críticas por comentários relacionados à cultura do cancelamento.",
+      "curiosidade": "Os fãs buscam por detalhes sobre o estilo retrô e cinematográfico de Lana Del Rey."
+    }
+  },
+  {
+    "id": "adele",
+    "nome": "Adele",
+    "shortDescription": "Cantora premiada, conhecida por álbuns de sucesso como '21' e '25'.",
+    "description": "Adele é uma cantora britânica de grande sucesso, conhecida por sua poderosa voz e letras emocionais. Ela ganhou inúmeros prêmios e alcançou sucesso comercial com álbuns como '21' e '25', estabelecendo-se como uma das artistas mais renomadas de sua geração.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "35",
+      "nacionalidade": "Britânica",
+      "areaDeAtuacao": "Música",
+      "trabalhoMaisRelevante": "Cantora premiada, destaque por álbuns como '21' e '25'.",
+      "escandalo": "Rumores sobre a vida pessoal.",
+      "curiosidade": "Voltou recentemente à cena musical após pausa."
+    }
+  },
+  {
+    "id": "serena-williams",
+    "nome": "Serena Williams",
+    "shortDescription": "Tenista lendária com múltiplos títulos de Grand Slam.",
+    "description": "Serena Williams é uma das tenistas mais bem-sucedidas de todos os tempos, conhecida por sua habilidade excepcional e dominância no esporte. Ela conquistou múltiplos títulos de Grand Slam e é reverenciada como uma das maiores atletas da história.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "40",
+      "nacionalidade": "Americana",
+      "areaDeAtuacao": "Esportes (Tênis)",
+      "trabalhoMaisRelevante": "Tenista lendária com múltiplos títulos de Grand Slam.",
+      "escandalo": "Polêmicas em competições.",
+      "curiosidade": "Empreendedora e mãe dedicada."
+    }
+  },
+  {
+    "id": "zendaya",
+    "nome": "Zendaya",
+    "shortDescription": "Atriz em séries como 'Euphoria' e presença marcante na moda.",
+    "description": "Zendaya é uma talentosa atriz americana, conhecida por seu papel em 'Euphoria' e por sua presença marcante na indústria da moda. Ela se destaca como uma voz influente entre os jovens e é admirada por sua autenticidade e compromisso com a diversidade.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "26",
+      "nacionalidade": "Americana",
+      "areaDeAtuacao": "Cinema e Moda",
+      "trabalhoMaisRelevante": "Atriz em séries como 'Euphoria' e presença marcante na moda.",
+      "escandalo": "Livre de grandes controvérsias públicas.",
+      "curiosidade": "Jovem ativista e defensora da diversidade."
+    }
+  },
+  {
+    "id": "jeff-bezos",
+    "nome": "Jeff Bezos",
+    "shortDescription": "Fundador da Amazon, um dos homens mais ricos do mundo.",
+    "description": "Jeff Bezos é um empresário americano conhecido por fundar a Amazon, uma das maiores empresas de comércio eletrônico do mundo. Ele também é fundador e financiador da empresa de exploração espacial Blue Origin e é considerado um dos homens mais ricos do mundo.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "60",
+      "nacionalidade": "Americana",
+      "areaDeAtuacao": "Ciência e Tecnologia (Empresário)",
+      "trabalhoMaisRelevante": "Fundador da Amazon, um dos homens mais ricos do mundo.",
+      "escandalo": "Questões legais e disputas de divórcio.",
+      "curiosidade": "Investidor em projetos espaciais com a empresa Blue Origin."
+    }
+  },
+  {
+    "id": "shakira",
+    "nome": "Shakira",
+    "shortDescription": "Cantora internacionalmente reconhecida, com hits como 'Hips Don't Lie'.",
+    "description": "Shakira é uma cantora e compositora colombiana de renome internacional, conhecida por seus sucessos como 'Hips Don't Lie' e 'Whenever, Wherever'. Ela também é reconhecida por seu trabalho filantrópico e é embaixadora da Boa Vontade da UNICEF.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "46",
+      "nacionalidade": "Colombiana",
+      "areaDeAtuacao": "Música",
+      "trabalhoMaisRelevante": "Cantora internacionalmente reconhecida, com hits como 'Hips Don't Lie'.",
+      "escandalo": "Questões legais envolvendo impostos.",
+      "curiosidade": "Além da música, envolvida em projetos de caridade."
+    }
+  },
+  {
+    "id": "tom-hanks",
+    "nome": "Tom Hanks",
+    "shortDescription": "Ator premiado em filmes como 'Forrest Gump' e 'Toy Story'.",
+    "description": "Tom Hanks é um ator americano amplamente reconhecido por sua versatilidade e talento. Ele estrelou em uma variedade de filmes aclamados pela crítica, incluindo 'Forrest Gump', 'Toy Story' e 'Saving Private Ryan', conquistando diversos prêmios ao longo de sua carreira.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "67",
+      "nacionalidade": "Americana",
+      "areaDeAtuacao": "Cinema",
+      "trabalhoMaisRelevante": "Ator premiado em filmes como 'Forrest Gump' e 'Toy Story'.",
+      "escandalo": "Sem grandes controvérsias públicas.",
+      "curiosidade": "Conhecido por sua carreira consistente e carisma."
+    }
+  },
+  {
+    "id": "malala-yousafzai",
+    "nome": "Malala Yousafzai",
+    "shortDescription": "Ativista pelos direitos das mulheres e educação, Prêmio Nobel da Paz.",
+    "description": "Malala Yousafzai é uma ativista paquistanesa conhecida por sua defesa dos direitos das mulheres e do acesso à educação. Ela ganhou destaque internacional após sobreviver a um atentado do Talibã em 2012 e continua sendo uma voz inspiradora e influente em todo o mundo.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "26",
+      "nacionalidade": "Paquistanesa",
+      "areaDeAtuacao": "Ativismo",
+      "trabalhoMaisRelevante": "Ativista pelos direitos das mulheres e educação, Prêmio Nobel da Paz.",
+      "escandalo": "Sem histórico de escândalos.",
+      "curiosidade": "Estudante universitária comprometida com a educação global."
+    }
+  },
+  {
+    "id": "mark-zuckerberg",
+    "nome": "Mark Zuckerberg",
+    "shortDescription": "Fundador do Facebook e CEO do Meta (anteriormente Facebook, Inc.).",
+    "description": "Mark Zuckerberg é um empresário e programador americano, conhecido por fundar o Facebook e liderar a empresa como seu CEO. Ele desempenhou um papel fundamental na expansão da plataforma e na transformação da maneira como as pessoas se comunicam e compartilham informações online.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "39",
+      "nacionalidade": "Americana",
+      "areaDeAtuacao": "Ciência e Tecnologia (Empresário)",
+      "trabalhoMaisRelevante": "Fundador do Facebook e CEO do Meta (anteriormente Facebook, Inc.).",
+      "escandalo": "Questões relacionadas à privacidade e segurança de dados.",
+      "curiosidade": ""
+    }
+  },
+  {
+    "id": "thiago-braz",
+    "nome": "Thiago Braz",
+    "shortDescription": "Medalhista de ouro olímpico.",
+    "description": "Thiago Braz é um atleta brasileiro especializado em salto com vara. Ele ganhou destaque internacional ao conquistar a medalha de ouro nos Jogos Olímpicos de 2016 no Rio de Janeiro, estabelecendo um novo recorde olímpico no processo.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "28",
+      "nacionalidade": "Brasileira",
+      "areaDeAtuacao": "Esportes (Atletismo - Salto com Vara)",
+      "trabalhoMaisRelevante": "Medalhista de ouro olímpico.",
+      "popularidadeEngajamentoRedes": "500 mil seguidores nas redes sociais",
+      "relacionamento": "Casado",
+      "escandalo": "Não há escândalos relevantes",
+      "curiosidade": "Quebrou o recorde olímpico em 2016."
+    }
+  },
+  {
+    "id": "sabrina-sato",
+    "nome": "Sabrina Sato",
+    "shortDescription": "Apresentadora.",
+    "description": "Sabrina Sato é uma personalidade da televisão brasileira, conhecida por seu trabalho como apresentadora em diversos programas de entretenimento. Ela ganhou destaque por sua espontaneidade e carisma, conquistando uma base sólida de fãs ao longo de sua carreira.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "41",
+      "nacionalidade": "Brasileira",
+      "areaDeAtuacao": "TV e Entretenimento",
+      "trabalhoMaisRelevante": "Apresentadora.",
+      "escandalo": "Participação polêmica em reality show",
+      "curiosidade": "Conhecida por seu carisma e espontaneidade."
+    }
+  },
+  {
+    "id": "wagner-moura",
+    "nome": "Wagner Moura",
+    "shortDescription": "Ator e diretor.",
+    "description": "Wagner Moura é um renomado ator e diretor brasileiro, reconhecido por seus papéis marcantes no cinema e na televisão. Ele ganhou destaque internacional por sua atuação como o narcotraficante colombiano Pablo Escobar na série de televisão 'Narcos'.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "46",
+      "nacionalidade": "Brasileira",
+      "areaDeAtuacao": "Cinema e TV",
+      "trabalhoMaisRelevante": "Ator e diretor",
+      "escandalo": "Críticas políticas durante discursos públicos",
+      "curiosidade": "Interpretou o papel de Pablo Escobar na série 'Narcos'."
+    }
+  },
+  {
+    "id": "marina-ruy-barbosa",
+    "nome": "Marina Ruy Barbosa",
+    "shortDescription": "Atriz e modelo.",
+    "description": "Marina Ruy Barbosa é uma talentosa atriz e modelo brasileira, reconhecida por seus papéis em novelas de televisão e por seu trabalho como modelo em campanhas publicitárias. Ela é uma presença constante em eventos de moda e tem uma forte influência nas redes sociais.",
+    "imageUrl": "",
+    "facts": {
+      "idade": "27",
+      "nacionalidade": "Brasileira",
+      "areaDeAtuacao": "TV e Moda",
+      "trabalhoMaisRelevante": "Atriz e modelo",
+      "escandalo": "Divórcio e rumores de relacionamento com colegas de trabalho",
+      "curiosidade": "Presença constante em eventos de moda."
+    }
+  }
+];
+
+  /*
   {
     "id": "ada-lovelace",
     "name": "Ada Lovelace",
--- src/view.js
+++ src/view.js
@@ -3,6 +3,7 @@ export const renderItems = (data) => {
   
   // Aquí comienza tu código y puedes retornar lo que tu necesites
   
+  
   return 'example';
 };
 
