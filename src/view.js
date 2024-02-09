8a88c9a880e4f842b42eb80fa23bfbdc0cc34a92
PVCBia <pvcbeatriz@gmail.com>
2024-02-09 13:16:37 -0300

    correção da parta test

--- src/view.js
+++ src/view.js
@@ -1,9 +1,30 @@
 export const renderItems = (data) => {
-  console.log(data)
-  
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  
-  
-  return 'example';
-};
+    console.log(data)
 
+    // Aquí comienza tu código y puedes retornar lo que tu necesites
+    /*let result = '<ul>'
+      data.array.forEach(element => {
+        result += `
+    <li>
+    <p>${elemento.name}</p>
+    </li>
+    `
+      });
+    result = result + '</ul>'
+
+  return 'result';
+  */
+  let result = '<ul>'
+  data.forEach(element => {
+  result += `
+    <li>
+    <p>${element.name}</p>
+    <img src= ${element.imageUrl} alt= "">
+    <p>${element.description}</p> 
+    </li>
+    `
+  });
+
+  result = result + '</ul>'
+
+  return result;
