74aa020d152e9b2bcccf4b8fdac7e34669004351
sellylima <vanessa.limanasc@gmail.com>
2024-02-09 11:20:32 -0300

    add stilo css

--- src/main.js
+++ src/main.js
@@ -10,3 +10,6 @@ import { renderItems } from './view.js';
 import data from './data/dataset.js';
 
 console.log(example, renderItems(data), data);
+
+const root = document.querySelector("#root");
+root.innerHTML = renderItems(data);
--- src/view.js
+++ src/view.js
@@ -2,8 +2,16 @@ export const renderItems = (data) => {
   console.log(data)
   
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  
-  
-  return 'example';
+  let result = <'ul'>
+  data.array.forEach(element => {
+    result += `
+    <li>
+    <p>${elemento.name}</p>
+    </li>
+    `
+  });
+   result = result + '</ul>'
+
+  return 'result';
 };
 
