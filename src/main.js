6b1531561b158efa34ac30242e832f95c5a2c539
PVCBia <pvcbeatriz@gmail.com>
2024-02-14 11:26:44 -0300

    inserção de funções para filtros, ordenação e escutador para botão de limpar

--- src/main.js
+++ src/main.js
@@ -1,36 +1,22 @@
-/*Recomendamos usar `src/main.js` para todo o seu código relacionado à exibição dos dados na tela.
-Basicamente, nos referimos à interação com o DOM. Operações como criação de nós, registro de 
-manipuladores de eventos (*event listeners* ou *event handlers*).
-Neste arquivo, você encontrará uma série de *imports* prontos para *carregar* as diferentes fontes
-de dados.*/
-
-import { example } from './dataFunctions.js';
+import { filterData, sortData, computeStats } from './dataFunctions.js';
 
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
-console.log(example, renderItems(data), data);
+//console.log(example, renderItems(data), data);
 
+const listaCards = document.querySelector('#root');
 
+const filtroNacionalidade = document.getElementById('filtroNacionalidade');
 
+const ordenação = document.getElementById('filtroOrdem');
 
- //botão de limpar  - escuta e limpa
-const limpaFiltros = document.querySelector('[data-testid="limpar"]');
-limpaFiltros.addEventListener('click', () => {
-    // Limpar todos os elementos <select>
-    document.querySelectorAll('select').forEach(select => {
-        select.value = '';
-    });
-});
+const limparSeleções = document.getElementById('botãoLimpar');
 
-//filtro areaDeAtuação - escuta mudanças e revela o valor
-const selecionaElemento = document.getElementById('areaDeAtuação');
-selecionaElemento.addEventListener('change', function (event) {
-    // Obtém o valor selecionado
-    const valorSelecionado = event.target.value;
-});
-
-const root = document.querySelector("#root")
-console.log(root)
-root.interHTML = renderItems(data)
+limparSeleções.addEventListener('click', () => {
+  listaCards.innerHTML = "";
+  ordenação.innerHTML = "";
+  filtroNacionalidade.innerHTML = "";
+  listaCards.appendChild(renderItems(data))
+})
