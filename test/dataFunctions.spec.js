553dfa6ea3977cd54724357a38f57df1f99a38af
PVCBia <pvcbeatriz@gmail.com>
2024-02-19 19:50:44 -0300

    inserção do teste para o filtro nacionalidade

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,28 +1,34 @@
-
-
-
-// esta função recebe a matriz de dados para renderizar os
+//esta função recebe a matriz de dados para renderizar os
 //elementos de cada item e deve retornar um elemento DOM ou uma string de HTML.
 
-renderItems(data) 
-
-
-
-import { example, anotherExample } from '../src/dataFunctions.js';
+import { filterData, sortData, computeStats } from '../src/dataFunctions.js';
 import { data as fakeData } from './data.js';
 
 console.log(fakeData);
 
-describe('example', () => {
+describe ('testes da função filterData', () => {
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('deve retornar 3 celebridades quando o usuário escolher brasileira', () => {
+    const dadosFiltrados = filterData(fakeData, 'nacionalidade', 'Brasileira');
+    expect(dadosFiltrados.length).toBe(3);
+  });
+
+  it('deve retornar 0 celebridades quando o usuário escolher cubana', () => {
+    const dadosFiltrados = filterData(fakeData, 'nacionalidade', 'Cubana');
+    expect(dadosFiltrados.length).toBe(0);
   });
 });
 
-describe('anotherExample', () => {
+describe('testes da função sortData', () => {
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('deve retornar o nome da função', () => {
+    expect(sortData()).toBe('sortData');
+  });
+});
+
+describe('testes da função computeStats', () => {
+
+  it('deve retornar o nome da função', () => {
+    expect(computeStats()).toBe('computeStats');
   });
 });
