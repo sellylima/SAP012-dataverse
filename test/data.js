2ccd2e04638652dd63cf0bfeef7bc94070f62a8c
PVCBia <pvcbeatriz@gmail.com>
2024-02-29 11:13:42 -0300

    insersão de dados para testes de ordenação

--- test/data.js
+++ test/data.js
@@ -2,52 +2,54 @@
 // al menos debe tener uno que se llame "data"
 
 export const data = [
-    {
-        "id": "thiago-braz",
-        "nome": "Thiago Braz",
-        "shortDescription": "Medalhista de ouro olímpico.",
-        "description": "Thiago Braz é um atleta brasileiro especializado em salto com vara. Ele ganhou destaque internacional ao conquistar a medalha de ouro nos Jogos Olímpicos de 2016 no Rio de Janeiro, estabelecendo um novo recorde olímpico no processo.",
-        "imageUrl": "",
-        "facts": {
-            "idade": "28",
-            "nacionalidade": "Brasileira",
-            "areaDeAtuacao": "Esportes (Atletismo - Salto com Vara)",
-            "trabalhoMaisRelevante": "Medalhista de ouro olímpico.",
-            "popularidadeEngajamentoRedes": "500 mil seguidores nas redes sociais",
-            "relacionamento": "Casado",
-            "escandalo": "Não há escândalos relevantes",
-            "curiosidade": "Quebrou o recorde olímpico em 2016."
-        }
+  {
+    id: "thiago-braz",
+    nome: "Thiago Braz",
+    shortDescription: "Medalhista de ouro olímpico.",
+    description:
+      "Thiago Braz é um atleta brasileiro especializado em salto com vara. Ele ganhou destaque internacional ao conquistar a medalha de ouro nos Jogos Olímpicos de 2016 no Rio de Janeiro, estabelecendo um novo recorde olímpico no processo.",
+    imageUrl: "",
+    facts: {
+      idade: "28",
+      nacionalidade: "Brasileira",
+      areaDeAtuacao: "Esportes (Atletismo - Salto com Vara)",
+      trabalhoMaisRelevante: "Medalhista de ouro olímpico.",
+      popularidadeEngajamentoRedes: "500 mil seguidores nas redes sociais",
+      relacionamento: "Casado",
+      escandalo: "Não há escândalos relevantes",
+      curiosidade: "Quebrou o recorde olímpico em 2016.",
     },
-    {
-        "id": "sabrina-sato",
-        "nome": "Sabrina Sato",
-        "shortDescription": "Apresentadora.",
-        "description": "Sabrina Sato é uma personalidade da televisão brasileira, conhecida por seu trabalho como apresentadora em diversos programas de entretenimento. Ela ganhou destaque por sua espontaneidade e carisma, conquistando uma base sólida de fãs ao longo de sua carreira.",
-        "imageUrl": "",
-        "facts": {
-            "idade": "41",
-            "nacionalidade": "Brasileira",
-            "areaDeAtuacao": "TV e Entretenimento",
-            "trabalhoMaisRelevante": "Apresentadora.",
-            "escandalo": "Participação polêmica em reality show",
-            "curiosidade": "Conhecida por seu carisma e espontaneidade."
-        }
+  },
+  {
+    id: "sabrina-sato",
+    nome: "Sabrina Sato",
+    shortDescription: "Apresentadora.",
+    description:
+      "Sabrina Sato é uma personalidade da televisão brasileira, conhecida por seu trabalho como apresentadora em diversos programas de entretenimento. Ela ganhou destaque por sua espontaneidade e carisma, conquistando uma base sólida de fãs ao longo de sua carreira.",
+    imageUrl: "",
+    facts: {
+      idade: "41",
+      nacionalidade: "Brasileira",
+      areaDeAtuacao: "TV e Entretenimento",
+      trabalhoMaisRelevante: "Apresentadora.",
+      escandalo: "Participação polêmica em reality show",
+      curiosidade: "Conhecida por seu carisma e espontaneidade.",
     },
-    {
-        "id": "wagner-moura",
-        "nome": "Wagner Moura",
-        "shortDescription": "Ator e diretor.",
-        "description": "Wagner Moura é um renomado ator e diretor brasileiro, reconhecido por seus papéis marcantes no cinema e na televisão. Ele ganhou destaque internacional por sua atuação como o narcotraficante colombiano Pablo Escobar na série de televisão 'Narcos'.",
-        "imageUrl": "",
-        "facts": {
-            "idade": "46",
-            "nacionalidade": "Brasileira",
-            "areaDeAtuacao": "Cinema e TV",
-            "trabalhoMaisRelevante": "Ator e diretor",
-            "escandalo": "Críticas políticas durante discursos públicos",
-            "curiosidade": "Interpretou o papel de Pablo Escobar na série 'Narcos'."
-        }
+  },
+  {
+    id: "wagner-moura",
+    nome: "Wagner Moura",
+    shortDescription: "Ator e diretor.",
+    description:
+      "Wagner Moura é um renomado ator e diretor brasileiro, reconhecido por seus papéis marcantes no cinema e na televisão. Ele ganhou destaque internacional por sua atuação como o narcotraficante colombiano Pablo Escobar na série de televisão 'Narcos'.",
+    imageUrl: "",
+    facts: {
+      idade: "46",
+      nacionalidade: "Brasileira",
+      areaDeAtuacao: "Cinema e TV",
+      trabalhoMaisRelevante: "Ator e diretor",
+      escandalo: "Críticas políticas durante discursos públicos",
+      curiosidade: "Interpretou o papel de Pablo Escobar na série 'Narcos'.",
     },
+  },
 ];
-
