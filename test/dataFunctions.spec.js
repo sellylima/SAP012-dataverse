5673d9831d45d8067451a06b282b91adaac6256e
Juliana Myaki <j.myaki@gmail.com>
2024-03-04 16:27:29 -0300

    Update dataFunctions.spec.js

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -8,27 +8,15 @@ console.log(fakeData);
 
 describe ('testes da função filterData', () => {
 
-  it('deve retornar 3 celebridades quando o usuário escolher brasileira', () => {
-    const dadosFiltrados = filterData(fakeData, 'nacionalidade', 'Brasileira');
-    expect(dadosFiltrados.length).toBe(3);
-  });
-
-  it('deve retornar 0 celebridades quando o usuário escolher cubana', () => {
-    const dadosFiltrados = filterData(fakeData, 'nacionalidade', 'Cubana');
-    expect(dadosFiltrados.length).toBe(0);
-  });
+ 
 });
 
 describe('testes da função sortData', () => {
 
-  it('deve retornar o nome da função', () => {
-    expect(sortData()).toBe('sortData');
-  });
+
 });
 
 describe('testes da função computeStats', () => {
 
-  it('deve retornar o nome da função', () => {
-    expect(computeStats()).toBe('computeStats');
-  });
+ 
 });
